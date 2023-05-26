1f70a841d9cd21ef91ba053cf3785e1cbce602a2
Danu Sabu <81710905+iamdanusabu@users.noreply.github.com>
2023-05-26 23:03:54 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,22 @@
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDcTyFqdWZMP7qNgrnYqcYZ2OIXMMHBG7g",
+    authDomain: "upload-e8fed.firebaseapp.com",
+    databaseURL: "https://upload-e8fed-default-rtdb.firebaseio.com",
+    projectId: "upload-e8fed",
+    storageBucket: "upload-e8fed.appspot.com",
+    messagingSenderId: "58607096448",
+    appId: "1:58607096448:web:1e38896d507d5cf0588c65",
+    measurementId: "G-NCHGTERL30"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 function patientLoginPage(){
   window.location="patientlogin.html";
   setTimeout('patientLoginPage()',500);
